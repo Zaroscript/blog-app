fe186cd62b89029a53d7129b6a514b57f3ca8502
Andrew Nashaat <166779741+Zaroscript@users.noreply.github.com>
2024-06-23 03:11:58 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import BlogList from "@/Components/BlogList";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import BlogList from "@/Components/BlogList"; // Adjust the import path as per your project structure
 
 interface Post {
   id: number;
@@ -7,26 +9,31 @@ interface Post {
   body: string;
 }
 
-const Home = ({ posts }: { posts: Post[] }) => {
+const Home: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        if (!res.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const fetchedPosts: Post[] = await res.json();
+        setPosts(fetchedPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
+
   return (
-    <main className="container max-w-[95%] py-8 flex justify-center items-center">
+    <main className="container max-sm:w-[95%] py-8 flex justify-center items-center">
       <BlogList posts={posts} />
     </main>
   );
-};
-
-export const getServerSideProps = async () => {
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    if (!res.ok) {
-      throw new Error("Failed to fetch posts");
-    }
-    const posts: Post[] = await res.json();
-    return { props: { posts } };
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return { props: { posts: [] } }; 
-  }
 };
 
 export default Home;
